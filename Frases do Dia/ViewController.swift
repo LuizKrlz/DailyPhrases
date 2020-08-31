c13489b896512af6c9afe7608ae10046316712bf
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2020-08-31 19:23:43 -0300

    finally app

--- Frases do Dia/ViewController.swift
+++ Frases do Dia/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fraseResultado: UILabel!
+    
+    @IBAction func geraFrase(_ sender: Any) {
+        var frases: [String] = []
+        frases.append("Se você traçar metas absurdamentes altas e falhar seu fracasso será muito melhor que o sucesso de muitos")
+        frases.append("Ter sucesso é falhar repetidamente, mas sem perder o entusiasmo")
+        frases.append("Não é o mais forte que sobrevive, nem o mais inteligente. Quem sobrevive é o mais disposto à mudança.")
+        
+        let indice = arc4random_uniform(3)
+        fraseResultado.text = frases[Int(indice)]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ README.md
@@ -0,0 +1,7 @@
+# Daily Phrases for IOS
+
+---
+
+This is a case of study on development for IOS. The Idea is an app to generate random phrases
+
+<img src="print1.png" />
